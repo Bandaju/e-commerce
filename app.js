ea96a736e73b0ed1707b399a2033b759bbeda224
Bandaju <muminkacmaz@gmail.com>
2022-10-20 21:22:00 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+$(".megamenu-menu-list-item").hover(
+  function () {
+    $(".menu-dropdown-megamenu").addClass("denemelik");
+  },
+  function () {
+    $(".menu-dropdown-megamenu").removeClass("denemelik");
+  }
+);
